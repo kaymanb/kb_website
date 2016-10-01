294035df4cf702b32fd87453ce0f3fb53f5205aa
Kayman Brusse <kaymanbursse@gmail.com>
2016-10-01 18:02:19 -0400

    Fixed some small bugs in the styling.

--- js/responsive.js
+++ js/responsive.js
@@ -5,10 +5,11 @@
 
 // Function for toggling menu in navbar.
 function toggle_navbar_menu() {
+    var nav_class = "navbar-container";
     var navbar = document.getElementById("top_navbar");
-    if (navbar.className == "navbar") {
+    if (navbar.className == nav_class) {
         navbar.className += " open";
     } else {
-        navbar.className = "navbar";
+        navbar.className = nav_class;
     }
 }
