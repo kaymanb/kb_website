6332a2524cb27a852332fb92c8884790714de66c
Kayman Brusse <kaymanbursse@gmail.com>
2016-10-02 14:08:34 -0400

    Fixed animation and proof-read content.

--- js/responsive.js
+++ js/responsive.js
@@ -3,13 +3,23 @@
  * Author: Kayman Brusse
  */
 
+var nav_class = "navbar-container";
+
 // Function for toggling menu in navbar.
 function toggle_navbar_menu() {
-    var nav_class = "navbar-container";
     var navbar = document.getElementById("top_navbar");
     if (navbar.className == nav_class) {
         navbar.className += " open";
     } else {
+        navbar.className = nav_class;
+    }
+}
+
+// Function for closing menu in navbar.
+function close_navbar_menu() {
+    var navbar = document.getElementById("top_navbar");
+    if (navbar.className == nav_class) {}
+    else {
         navbar.className = nav_class;
     }
 }
